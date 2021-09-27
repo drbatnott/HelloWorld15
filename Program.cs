6bb84db3d485e6f1e66f7eeecbeb8e1d9a1d7488
drbatnott <zygomy@gmail.com>
2021-09-27 16:57:07 +0100

    We have added if statements and comments and more useful variable name

--- Program.cs
+++ Program.cs
@@ -8,12 +8,34 @@ namespace HelloWorld15
 {
     class Program
     {
+       /*
+        * 
+        * Program to introduce people to C#
+        * Uses string and integer variables
+        * It introduces the if statement
+        */
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World");
             System.Console.Write("Please Enter your name: ");
-            string t = System.Console.ReadLine();
-            System.Console.WriteLine("Hello " + t);
+            string sName = System.Console.ReadLine();
+            System.Console.WriteLine("Hello " + sName);
+            System.Console.Write("How old are you " + sName + ": ");
+            int age = 0;
+            string s = System.Console.ReadLine();
+            // If the string entered contains a . then its not an integer
+            if (s.Contains("."))
+            {
+                //The Double.Parse will return a double so if we want an integer
+                double d = Double.Parse(s);
+                //we need to cast it as an int - (int)
+                age = (int) System.Math.Round(d);
+            }
+            else //So if it doesn't contain . assume it is an integer
+            {
+                age = Int16.Parse(s);
+            }           
+            System.Console.WriteLine("Are you really " + age + " " + sName);
             System.Console.ReadLine();
         }
     }
