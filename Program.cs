715cd28e1cf2a905d818c0758f7edf3d131f483c
drbatnott <zygomy@gmail.com>
2021-09-27 15:25:57 +0100

    Changed Variable name as an example

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ namespace HelloWorld15
         {
             System.Console.WriteLine("Hello World");
             System.Console.Write("Please Enter your name: ");
-            string s = System.Console.ReadLine();
-            System.Console.WriteLine("Hello " + s);
+            string t = System.Console.ReadLine();
+            System.Console.WriteLine("Hello " + t);
             System.Console.ReadLine();
         }
     }
